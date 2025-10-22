8a0e74bfc1933d19be3242b355f2f15c6bcc675a
VeronikaMyts <152305850+VeronikaMyts@users.noreply.github.com>
2025-10-22 12:43:37 +0200

    Add files via upload

---
+++ oipz/іопз лаба3/іопз лаба3/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace іопз_лаба3
+{
+     class Class1
+    {
+        public static double sinus(double a, double c)
+        {
+            double sinusA;
+            if (c != 0 && a != 0 && c > a)
+            {
+                sinusA = a / c;
+                return sinusA;
+            }
+            else
+            {
+                MessageBox.Show("Невірно введені дані. ");
+                return 0;
+            }
+
+        }
+        public static double mediana(double a, double c)
+        {
+            double medianaB;
+            if (c != 0 && a != 0 && c > a)
+            {
+                medianaB = (Math.Sqrt(Math.Abs(3 * Math.Pow(a, 2) - Math.Pow(c, 2)))) / 2;
+                return medianaB;
+            }
+            else
+            {
+                MessageBox.Show("Невірно введені дані. ");
+                return 0;
+            }
+
+        }
+        public static double area(double a, double b, double c)
+        {
+            double S;
+            if (c != 0 && a != 0 && b != 0)
+            {
+                S = ((a + b) * Math.Sqrt(Math.Abs(4 * Math.Pow(c, 2) - Math.Pow((a - b), 2)))) / 2;
+                return S;
+            }
+            else
+            {
+                MessageBox.Show("Невірно введені дані. ");
+                return 0;
+            }
+
+        }
+        public static double angel(double a, double b, double c)
+        {
+            double angelA;
+            if (c != 0 && a != 0 && b != 0 && b < c)
+            {
+                angelA = (Math.Acos(a - b)) / 2 * c;
+                return angelA;
+            }
+            else
+            {
+                MessageBox.Show("Невірно введені дані. ");
+                return 0;
+            }
+
+        }
+    }
+}
+
---
+++ oipz/іопз лаба3/іопз лаба3/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace іопз_лаба3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double c, a, s, m;
+            if (String.IsNullOrEmpty(textBox1.Text) || String.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Стрічки не повинні бути пусті ");
+
+            }
+            
+                c = Convert.ToDouble(textBox1.Text);
+            a = Convert.ToDouble(textBox2.Text);
+            s = Class1.sinus(a, c);
+            m = Class1.mediana(a, c);
+               
+            if (s != 0)
+            {
+
+
+
+                label3.Text = "Синус гострого кута = " + s.ToString() + " градусів.";
+
+            }
+            else
+            {
+                label3.Text = "Не вдалося обчислити величину кута.";
+
+            }
+            if (m != 0)
+            {
+                label4.Text = "Медіана  = " + m.ToString() + " см.";
+            }
+            else
+            {
+                label4.Text = "Не вдалося обчисли медіану";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double a, b, c, S, K;
+            a = Convert.ToDouble(textBox3.Text);
+            b = Convert.ToDouble(textBox4.Text);
+            c = Convert.ToDouble(textBox5.Text);
+            S = Class1.area(a, b, c);
+            K = Class1.angel(a, b, c);
+            if (S != 0)
+            {
+
+
+
+                label7.Text = "Площа  = " + S.ToString() + " см^2.";
+
+            }
+            else
+            {
+                label7.Text = "Не вдалося обчислити площу.";
+
+            }
+            if (K != 0)
+            {
+                label8.Text = "Гострий кут  = " + K.ToString() + " градусах.";
+            }
+            else
+            {
+                label8.Text = "Не вдалося обчисли гострий кут";
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsNumber(e.KeyChar) && e.KeyChar != ',' && e.KeyChar != 45 && !Char.IsControl(e.KeyChar))
+                e.Handled = true;
+        }
+    }
+}
+    
+
+    
+
